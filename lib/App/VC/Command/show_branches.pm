8c41730b8ec6566805f2174ad88a7ab22f6e3f10
Buddy Burden <barefootcoder@gmail.com>
2013-08-30 18:18:03 -0700

    new command: vc show-branches

---
+++ lib/App/VC/Command/show_branches.pm
@@ -0,0 +1,49 @@
+use 5.012;
+use autodie qw< :all >;
+use warnings FATAL => 'all';
+
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
+
+
+class App::VC::Command::show_branches extends App::VC::Command
+{
+	use Path::Class;
+	use MooseX::Has::Sugar;
+	use MooseX::Types::Moose qw< :all >;
+
+
+	method description
+	{
+		return	"\n"
+			.	"List all known branches.\n"
+			.	"\n"
+			;
+	}
+
+	override command_names
+	{
+		return 'show-branches', super();
+	}
+
+
+	method validate_args ($opt, ArrayRef $args)
+	{
+		$self->verify_project;
+	}
+
+	augment execute (...)
+	{
+	}
+}
+
+
+1;
+
+
+=head1 NAME
+
+App::VC::Command::show_branches - list branches in working copy
+
+
+=cut
