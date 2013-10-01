8904d2ce39c2a6a53b5317f973cfe13830ae26f1
Buddy Burden <barefootcoder@gmail.com>
2013-09-30 18:51:53 -0700

    can now create a rudimentary config file if you don't have one
    includes an auto-discovery mode and a manual mode with guided question-answer
    note that this pretends like it will understand Suversion and CVS repos, which it doesn't really yet
    -	(but at least you can get them in your config)
    -	(of course, it will make your config blow up, but, hey: nothing's perfect)

--- lib/App/VC/Command.pm
+++ lib/App/VC/Command.pm
@@ -9,6 +9,7 @@ use MooseX::Attribute::ENV;
 
 class App::VC::Command extends MooseX::App::Cmd::Command
 {
+	use TryCatch;
 	use Debuggit;
 	use autodie qw< :all >;
 	use experimental 'smartmatch';
@@ -97,11 +98,22 @@ class App::VC::Command extends MooseX::App::Cmd::Command
 		my $home = File::HomeDir->my_home;
 		my $config_file = file($home, '.vctools.conf');
 
-		# a small bit of pre-processing to allow ~ to refer to the user's home directory
-		# but only for *Dir directives, or in <<include>> statements
-		my $raw_config = slurp "$config_file";							# quotes to remove Path::Class magic
-		$raw_config =~ s{ ^ (\s* \w+Dir \s* = \s*) ~/ }{ $1 . $home . '/' }gmex;
-		$raw_config =~ s{ ^ (\s* << \s* include \s+) ~/ }{ $1 . $home . '/' }gmex;
+		my $raw_config;
+		try
+		{
+			$raw_config = slurp "$config_file";							# quotes to remove Path::Class magic
+
+			# a small bit of pre-processing to allow ~ to refer to the user's home directory
+			# but only for *Dir directives, or in <<include>> statements
+			$raw_config =~ s{ ^ (\s* \w+Dir \s* = \s*) ~/ }{ $1 . $home . '/' }gmex;
+			$raw_config =~ s{ ^ (\s* << \s* include \s+) ~/ }{ $1 . $home . '/' }gmex;
+		}
+		catch ($e where {/Can't open '$config_file'/})
+		{
+			$self->warning("config file not found; trying to create");
+			system(file($0)->dir->file('vctools-create-config'));
+			$self->fatal("If config file was successfully created, try your command again.");
+		}
 
 		my $config = { Config::General::ParseConfig( -String => $raw_config ) };
 		debuggit(3 => "read config:", DUMP => $config);
@@ -362,6 +374,11 @@ class App::VC::Command extends MooseX::App::Cmd::Command
             return $msg;
         }
     }
+
+	method warning ($msg)
+	{
+		say $self->me . ' ' . $self->command . ': ' . $self->color_msg(yellow => $msg);
+	}
 
 	method fatal ($msg)
 	{
