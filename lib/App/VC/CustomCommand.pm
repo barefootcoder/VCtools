704de9b932427785f18b5843c727fc2505b28868
Buddy Burden <barefootcoder@gmail.com>
2013-10-02 17:25:20 -0700

    further refactoring: now the spec is a separate object
    the command was just trying to do too much:
    -	it was trying to be the _spec_ for the command at the same time it was trying to actually _execute_ the command
    this is much cleaner
    plus it will enable me to generate the usage messages from the _spec_, which can exist before the command is instantiated
    but it's also much better separation of concerns

--- lib/App/VC/CustomCommand.pm
+++ lib/App/VC/CustomCommand.pm
@@ -6,7 +6,7 @@ use MooseX::Declare;
 use Method::Signatures::Modifiers;
 
 
-class App::VC::CustomCommand extends App::VC::Command is mutable
+class App::VC::CustomCommand extends App::VC::Command is mutable		# see BUILDARGS for why it's mutable
 {
 	use Debuggit;
 	use autodie qw< :all >;
@@ -16,130 +16,69 @@ class App::VC::CustomCommand extends App::VC::Command is mutable
 	use MooseX::Has::Sugar;
 	use MooseX::Types::Moose qw< :all >;
 
+	use App::VC::CustomCommandSpec;
+
 
 	# ATTRIBUTES
 
-	has _command_names	=>	( ro, isa => ArrayRef, writer => '_set_command_names' );
-	has arguments		=>	( ro, isa => ArrayRef, writer => '_set_arguments' );
+	has spec			=>	( ro, isa => 'App::VC::CustomCommandSpec', writer => '_set_spec' );
 
-	has min_files		=>	( ro, isa => Int, writer => '_set_min_files' );
-	has max_files		=>	( ro, isa => Int, writer => '_set_max_files' );
 	has _files			=>	(
 								traits => [qw< Array >],
 									handles => { files => 'elements' },
 								ro, isa => ArrayRef[Str], writer => '_set_files',
 							);
 
-	has action			=>	( ro, isa => Str, writer => '_set_action' );
 
-	# validations
-	my %VALIDATIONS =
-	(
-		project		=>	'Bool',
-	);
-	while (my ($att, $type) = each %VALIDATIONS)
+	# PSEUDO-ATTRIBUTES
+
+	method command_names ($invocant: )
 	{
-		has "should_verify_$att" => ( ro, isa => $type, writer => "_set_verify_$att", );
+		if (ref $invocant)												# we have an actual object
+		{
+			return @{ $invocant->_command_names };
+		}
+		else															# classname: nothing useful to return here
+		{
+			return ( '???' );
+		}
+	}
+
+	override usage_desc ($class: ...)
+	{
+		return super();
 	}
 
 
 	# CONSTRUCTOR
 
-	# Call this instead of prepare (it calls prepare for you).
-	# Basically, this constructs an App::Cmd::Command object for you like normal (i.e., by calling
-	# prepare()), then adds a bunch of stuff to the object before passing it back.  This includes
-	# adding some attributes on-the-fly, which is a _bit_ odd, but not too terrbily meddlesome.  (At
-	# least, all the other ways I tried to do this were _way_ worse.)  Since most of the extra
-	# attributes (defined above) have to be set after the object is initially constructed, they're
-	# all read-only with private mutators.  The alternative would have been to somehow hook into
-	# App::Cmd::ArgProcessor::_process_args, but I felt that would rapidly get too messy.  *Plus* it
-	# would have put me back into the position of having to report fatal errors without having an
-	# object to call fatal() on yet.  So, it's not a *super* clean design, but it's clear and
-	# workable.  Well, clear enough once you understand the guts of how App::Cmd works, which is
-	# admittedly a daunting proposition.
+	# Call this instead of prepare.  Basically, this constructs an App::Cmd::Command object for you
+	# like normal (i.e., by calling prepare() for you), then adds more stuff to the object before
+	# passing it back.  This includes adding some attributes on-the-fly, which is a _bit_ odd, but
+	# not too terrbily meddlesome.  (At least, all the other ways I tried to do this were _way_
+	# worse.)  It also saves the custom spec object to the custom command object.  Since the spec
+	# has to be set after the object is initially constructed, it's read-only with a private mutator
+	# (instead of being read-only and required).  The alternative would have been to somehow hook
+	# into App::Cmd::ArgProcessor::_process_args (where I could have tacked on extra ctor
+	# parameters), but I felt that would get too messy.  So, it's not a *super* clean design, but
+	# it's clear and workable.  Well, clear enough once you understand the guts of how App::Cmd
+	# works, which is admittedly a daunting proposition.
 	method prepare_custom ($class: App::VC $app, Str $command, HashRef $spec, @args)
 	{
-		# go ahead and build the object first so we can use its fatal method if we hit a snag
+		my $spec_obj = App::VC::CustomCommandSpec->new( $command, $spec );
+
 		my ($self, $opt, @cmd_args) = $class->prepare($app, @args);
+		$self->fatal($spec_obj->fatal_error) if $spec_obj->has_fatal_error;
 
-		# command names
-		#$self->{'_command_names'} = [ $command ];						# maybe allow aliases in spec?
-
-		# validations
-		if (exists $spec->{'Verify'})
-		{
-			my $validate = $spec->{'Verify'};
-			if (!ref $validate)											# it's a scalar; only one validation
-			{
-				$validate = [ $validate ];
-			}
-			elsif (ref $validate ne 'ARRAY')							# then it's something bogus
-			{
-				$self->fatal("Config file error: Verify spec for CustomCommand $command");
-			}
-
-			# at this point, we're sure it's an arrayref, so loop through it
-			foreach (@$validate)
-			{
-				if (exists $VALIDATIONS{$_})
-				{
-					my $verify_method = "_set_verify_$_";
-					$self->$verify_method(1);
-				}
-				else
-				{
-					$self->fatal("Config file error: Verify spec for CustomCommand $command (`$_' unknown)");
-				}
-			}
-		}
-
-		# arguments
-		my $arguments;
-		if (not exists $spec->{'Argument'})
-		{
-			$arguments = [];
-		}
-		elsif (!ref $spec->{'Argument'})								# it's a scalar: only one argument
-		{
-			$arguments = [ $spec->{'Argument'} ];
-		}
-		elsif (ref $spec->{'Argument'} eq 'ARRAY')						# multiple arguments in an arrayref
-		{
-			$arguments = $spec->{'Argument'};
-		}
-		else															# don't know WTF it is ...
-		{
-			$self->fatal("Config file error: Argument spec for CustomCommand $command");
-		}
-		$self->_set_arguments($arguments);
 		# add any arguments as new on-the-fly attributes
-		$self->meta->add_attribute($_ => (ro, writer => "_set_$_")) foreach @$arguments;
-		# not going to make this immutable, surprisingly
+		$self->meta->add_attribute($_ => (ro, writer => "_set_$_")) foreach $spec_obj->arguments;
+		# not going to make the class immutable afterwards, surprisingly
 		# the primary benefit of immutable is to inline the ctor
 		# but we've already constructed the only instance we're ever going to build
 		# so make_immutable just takes time and gains no real benefit
 
-		# files
-		my ($min_files, $max_files);
-		if (not exists $spec->{'Files'})								# command takes no files at all
-		{
-			$min_files = $max_files = 0;
-		}
-		else
-		{
-			unless ( $spec->{'Files'} =~ /^ (\d+) ( \.\. (\d+)? )? $/x )
-			{
-				$self->fatal("Config file error: Files spec for CustomCommand $command");
-			}
-			$min_files = $1;
-			# with .. -> max is either the number provided, or -1 (meaning Inf); without .. -> max is same as min
-			$max_files = $2 ? $3 // -1 : $1;
-		}
-		$self->_set_min_files($min_files);
-		$self->_set_max_files($max_files);
-
-		# action
-		$self->_set_action($spec->{'action'}) or $self->fatal("Config file error: action spec for CustomCommand $command");
+		# save the spec
+		$self->_set_spec($spec_obj);
 
 		debuggit(2 => "custom command", $command, DUMP => $self);
 		return ($self, $opt, @cmd_args);
@@ -159,31 +98,13 @@ class App::VC::CustomCommand extends App::VC::Command is mutable
 
 	method validate_args ($opt, ArrayRef $args)
 	{
-		$self->verify_project if $self->should_verify_project;
-
-		foreach (@{ $self->arguments })
-		{
-			$self->fatal("Did not receive $_ argument.") unless @$args;
-
-			my $writer = "_set_$_";
-			$self->$writer(shift @$args);
-		}
-
-		if (@$args < $self->min_files or $self->max_files != -1 && @$args > $self->max_files)
-		{
-			my $proper_number = $self->max_files == -1
-					? join(' ', $self->min_files, "or more")
-					: join(' ', "between", $self->min_files, "and", $self->max_files);
-			$self->fatal("Wrong number of files: must be $proper_number");
-		}
-		$self->_set_files($args);
-
+		$self->spec->validate_args($self, $args);
 		debuggit(4 => "after validations", DUMP => $self);
 	}
 
 	method execute (...)
 	{
-		my @commands = $self->command_lines(custom => $self->action);
+		my @commands = $self->command_lines(custom => $self->spec->action);
 		$self->_process_cmdline(output => $_) or exit foreach @commands;
 	}
 }
---
+++ lib/App/VC/CustomCommandSpec.pm
@@ -0,0 +1,180 @@
+use 5.012;
+use autodie qw< :all >;
+use warnings FATAL => 'all';
+
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
+
+
+class App::VC::CustomCommandSpec
+{
+	use Debuggit;
+	use autodie qw< :all >;
+	use experimental 'smartmatch';
+
+	use Path::Class;
+	use MooseX::Has::Sugar;
+	use MooseX::Types::Moose qw< :all >;
+
+
+	# ATTRIBUTES
+
+	has _command_names	=>	(
+								traits => [qw< Array >],
+									handles => { command_names => 'elements' },
+								ro, isa => ArrayRef[Str], required, init_arg => 'command_names',
+							);
+	has _arguments		=>	(
+								traits => [qw< Array >],
+									handles => { arguments => 'elements' },
+								ro, isa => ArrayRef, required, init_arg => 'arguments',
+							);
+
+	has min_files		=>	( ro, isa => Int, required, );
+	has max_files		=>	( ro, isa => Int, required, );
+
+	has action			=>	( ro, isa => Str, required, );
+
+	has fatal_error		=>	( ro, isa => Str, init_arg => 'fatal', predicate => 'has_fatal_error' );
+
+	# validations
+	my %VALIDATIONS =
+	(
+		project		=>	'Bool',
+	);
+	while (my ($att, $type) = each %VALIDATIONS)
+	{
+		has "should_verify_$att" => ( ro, isa => $type, required, );
+	}
+
+
+	# PSEUDO-ATTRIBUTES
+
+	method command
+	{
+		return ($self->command_names)[0];
+	}
+
+	method usage_desc
+	{
+		return join(' ', '%c', $self->command, '%o');
+	}
+
+
+	# CONSTRUCTOR
+
+	around BUILDARGS ($class: Str $command, HashRef $spec)
+	{
+		my $args = {};
+		my $fatal_error;												# if we find one, store it for later reportage
+
+		# command names
+		$args->{'command_names'} = [ $command ];						# maybe allow aliases in spec?
+
+		# validations
+		if (exists $spec->{'Verify'})
+		{
+			my $validate = $spec->{'Verify'};
+			if (!ref $validate)											# it's a scalar; only one validation
+			{
+				$validate = [ $validate ];
+			}
+			elsif (ref $validate ne 'ARRAY')							# then it's something bogus
+			{
+				$fatal_error //= "Verify spec for CustomCommand $command";
+			}
+
+			# at this point, we're sure it's an arrayref, so loop through it
+			foreach (@$validate)
+			{
+				if (exists $VALIDATIONS{$_})
+				{
+					$args->{"should_verify_$_"} = 1;
+				}
+				else
+				{
+					$fatal_error //= "Verify spec for CustomCommand $command (`$_' unknown)";
+				}
+			}
+		}
+
+		# arguments
+		if (not exists $spec->{'Argument'})
+		{
+			$args->{'arguments'} = [];
+		}
+		elsif (!ref $spec->{'Argument'})								# it's a scalar: only one argument
+		{
+			$args->{'arguments'} = [ $spec->{'Argument'} ];
+		}
+		elsif (ref $spec->{'Argument'} eq 'ARRAY')						# multiple arguments in an arrayref
+		{
+			$args->{'arguments'} = $spec->{'Argument'};
+		}
+		else															# don't know WTF it is ...
+		{
+			$fatal_error //= "Argument spec for CustomCommand $command";
+		}
+
+		# files
+		if (not exists $spec->{'Files'})								# command takes no files at all
+		{
+			$args->{'min_files'} = $args->{'max_files'} = 0;
+		}
+		else
+		{
+			unless ( $spec->{'Files'} =~ /^ (\d+) ( \.\. (\d+)? )? $/x )
+			{
+				$fatal_error //= "Files spec for CustomCommand $command";
+			}
+			$args->{'min_files'} = $1;
+			# with .. -> max is either the number provided, or -1 (meaning Inf); without .. -> max is same as min
+			$args->{'max_files'} = $2 ? $3 // -1 : $1;
+		}
+
+		# action
+		$args->{'action'} = $spec->{'action'} or $fatal_error //= "action spec for CustomCommand $command";
+
+		# if we had an error, better add that in
+		$args->{'fatal'} = "Config file error: $fatal_error" if $fatal_error;
+
+		return $args;
+	}
+
+
+	# METHODS
+
+	method description
+	{
+		return	"\n"
+			.	"FILL ME IN.\n"
+			.	"\n"
+			;
+	}
+
+
+	method validate_args (App::VC::CustomCommand $cmd, ArrayRef $args)
+	{
+		$cmd->verify_project if $self->should_verify_project;
+
+		foreach ($self->arguments)
+		{
+			$cmd->fatal("Did not receive $_ argument.") unless @$args;
+
+			my $writer = "_set_$_";
+			$cmd->$writer(shift @$args);
+		}
+
+		if (@$args < $self->min_files or $self->max_files != -1 && @$args > $self->max_files)
+		{
+			my $proper_number = $self->max_files == -1
+					? join(' ', $self->min_files, "or more")
+					: join(' ', "between", $self->min_files, "and", $self->max_files);
+			$cmd->fatal("Wrong number of files: must be $proper_number");
+		}
+		$cmd->_set_files($args);
+	}
+}
+
+
+1;
