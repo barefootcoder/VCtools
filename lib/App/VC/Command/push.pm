9c74b2f5cf9886b72ce674d6ffb75a210dc33f5f
Buddy Burden <barefootcoder@gmail.com>
2013-12-31 15:16:03 -0800

    finally got around to creating `vc push`

---
+++ lib/App/VC/Command/push.pm
@@ -0,0 +1,43 @@
+use 5.012;
+use autodie qw< :all >;
+use warnings FATAL => 'all';
+
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
+
+
+class App::VC::Command::push extends App::VC::Command
+{
+	use Debuggit;
+	use autodie qw< :all >;
+	use experimental 'smartmatch';
+
+	use Path::Class;
+	use MooseX::Has::Sugar;
+	use MooseX::Types::Moose qw< :all >;
+
+
+	method abstract { "FILL ME IN" }
+
+	method description
+	{
+		return	"\n"
+			.	"FILL ME IN.\n"
+			.	"\n"
+			;
+	}
+
+
+	augment validate_args ($opt, ArrayRef $args)
+	{
+		$self->verify_project;
+		$self->fatal("Cannot push individual files") if @$args;
+	}
+
+	augment execute (...)
+	{
+	}
+}
+
+
+1;
